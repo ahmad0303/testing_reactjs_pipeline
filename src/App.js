1b9d5f684188529051cba5d9200702b997ac0622
Ahmad <realahmad001@gmail.com>
2025-01-01 18:43:55 +0500

    chng commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p style={{ color: "red" }}>
-          Edit <code>src/App.js</code> and save to ........
+          Edit <code>src/App.js</code> and save tooo.
         </p>
         <a
           className="App-link"
